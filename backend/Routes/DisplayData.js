7d31ceaa2c9d1fdbf932cdfc21d6998882c750a4
Joya18Shamim <shamimjoya10@gmail.com>
2024-07-21 23:32:52 +0530

    production ready

--- backend/Routes/DisplayData.js
+++ backend/Routes/DisplayData.js
@@ -8,7 +8,7 @@ router.post('/getlocation', async (req, res) => {
         let long = req.body.latlong.long
         console.log(lat, long)
         let location = await axios
-            .get("https://api.opencagedata.com/geocode/v1/json?q=" + lat + "+" + long + "&key=74c89b3be64946ac96d777d08b878d43")
+            .get(`https://api.opencagedata.com/geocode/v1/json?q=${lat}+${long}&key=74c89b3be64946ac96d777d08b878d43`)
             .then(async res => {
                 // console.log(`statusCode: ${res.status}`)
                 console.log(res.data.results)
@@ -17,7 +17,7 @@ router.post('/getlocation', async (req, res) => {
                 let response = res.data.results[0].components;
                 console.log(response)
                 let { village, county, state_district, state, postcode } = response
-                return String(village + "," + county + "," + state_district + "," + state + "\n" + postcode)
+                return String(village + "," + county + "," + state_district + "," + state + "," + postcode)
             })
             .catch(error => {
                 console.error(error)
